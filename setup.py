6d4d5df3dadfd56970737b9d56542db8cee40c4f
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-05-30 16:15:08 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
-from simplegithub import __version__
 
 setup(name='simplegithub.py',
       author='nerdguyahmad',
       url='https://github.com/nerdguyahmad/simple-github.py',
-      version=__version__,
-      packages=find_packages(include=['simplegithub', 'simplegithub.*']),
+      version='v1.0.0',
+      packages='simplegithub',
       license='MIT',
       description='A highly simple (experimental) wrapper for basic operations with GitHub API.',
       install_requires=['requests'],
